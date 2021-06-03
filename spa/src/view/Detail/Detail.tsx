504d17eef2c5db280c5cb3a942b03a9f294a6926
CORP\tnguyen4 <tan.b.nguyen@capgemini.com>
2021-06-03 17:28:42 +0200

    Detail page done

--- spa/src/view/Detail/Detail.tsx
+++ spa/src/view/Detail/Detail.tsx
@@ -1,21 +1,56 @@
 import React from "react";
 import { props } from "ramda";
 import { useParams } from "react-router-dom";
-
+import Paper from '@material-ui/core/Paper';
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
 import { example } from "./../Application/List";
 
+export interface Props {}
 
-// interface MatchParams {
-//   id: string;
-// }
-
-export interface Props { }
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      '& > *': {
+        margin: "auto",
+        width: theme.spacing(32),
+        height: theme.spacing(32),
+        "text-align": "center",
+      },
+    },
+  }),
+);
 
 export const Detail: React.FunctionComponent<Props> = () => {
   const id = useParams();
-  console.log(id);
-  console.log(example);
+  const classes = useStyles();
+
+  var s = JSON.stringify(id);
+  let idString: string = "";
+  let size = s.length;
+  let idNbr = 0;
+
+  function stringToNumber(sizeId: number) {
+    console.log(s.substr(7, sizeId));
+    idString = s.substr(7, sizeId);
+    idNbr = Number(idString);
+  }
+
+  if (size == 10) {
+    stringToNumber(1);
+  } else if (size == 11) {
+    stringToNumber(2);
+  } else {
+    stringToNumber(3);
+  }
+
   return (
-      <div>Detail Page du produit</div>
-  )
+    <div className={classes.root} style={{margin : "auto"}}>
+      <Paper elevation={3}>
+      <h1>Mot : <b>{example[idNbr].mot}</b></h1>
+      <h1>Traduction : <b id="red">{example[idNbr].traduction}</b></h1>
+      </Paper>
+    </div>
+  );
 };
